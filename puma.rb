62c3fda1ea061aef45b9792def8d32a33eeecb42
Johannes Opper <johannes.opper@gmail.com>
2016-05-13 11:01:21 +0200

    Tweaked puma settings

--- puma.rb
+++ puma.rb
@@ -1,5 +1,5 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 3)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+workers Integer(ENV['WEB_CONCURRENCY'] || 5)
+threads_count = Integer(ENV['MAX_THREADS'] || 32)
 threads threads_count, threads_count
 
 preload_app!
